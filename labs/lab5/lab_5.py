3afecce14105e53c224f5960affa66cf796bc3d7
Chukaev Roman <roman.chukaev1@gmail.com>
2025-11-08 13:05:41 +0700

    finish lab5

--- labs/lab5/lab_5.py
+++ labs/lab5/lab_5.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key = os.getenv('flask_secret', 'secret-key')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -21,7 +22,7 @@ class User(UserMixin):
 def get_db_connection():
     return psycopg2.connect(
         host='localhost',
-        database='lab5_db',
+        database='lab5_rpp_db',
         user='postgres',
         password=os.getenv('5lb_db_password', 'password')
     )
@@ -63,9 +64,13 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
+    email_value = ""
+
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        email_value = email
 
         conn = get_db_connection()
         cur = conn.cursor()
@@ -75,45 +80,51 @@ def login():
         conn.close()
 
         if not user_data:
-            return "Пользователь не найден"
+            error = "Пользователь не найден"
+        elif user_data[2] != password:
+            error = "Неверный пароль"
+        else:
+            user = User(user_data[0], user_data[1], user_data[2], user_data[3])
+            login_user(user)
+            return redirect('/')
 
-        if user_data[2] != password:
-            return "Неверный пароль"
-
-        user = User(user_data[0], user_data[1], user_data[2], user_data[3])
-        login_user(user)
-        return redirect('/')
-
-    return render_template('login.html')
+    return render_template('login.html', error=error, email=email_value)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = None
+    name_value = ""
+    email_value = ""
+
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
+
+        name_value = name
+        email_value = email
 
         conn = get_db_connection()
         cur = conn.cursor()
 
         cur.execute('SELECT * FROM users WHERE email = %s', (email,))
         if cur.fetchone():
+            error = "Пользователь с таким email уже существует"
+        else:
+            cur.execute(
+                'INSERT INTO users (email, password, name) VALUES (%s, %s, %s)',
+                (email, password, name)
+            )
+            conn.commit()
             cur.close()
             conn.close()
-            return "Пользователь уже существует"
+            return redirect('/login')
 
-        cur.execute(
-            'INSERT INTO users (email, password, name) VALUES (%s, %s, %s)',
-            (email, password, name)
-        )
-        conn.commit()
         cur.close()
         conn.close()
 
-        return redirect('/login')
-
-    return render_template('signup.html')
+    return render_template('signup.html', error=error, name=name_value, email=email_value)
 
 @app.route('/logout')
 def logout():
